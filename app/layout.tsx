9a135b5f9d372af6c963d8350b2429c8afd05f79
Sairainc <sairatechcompany@gmail.com>
2024-11-03 15:38:22 +0900

    ファビコン設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: '株式会社Saira',
   description: 'ビジネスの成長を加速するノーコードソリューションと人材戦略',
+  icons: {
+    icon: '/images/saira-high-resolution-logo-transparent.png',
+    apple: '/images/saira-high-resolution-logo-transparent.png',
+  },
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
       <body className={inter.className}>{children}</body>
